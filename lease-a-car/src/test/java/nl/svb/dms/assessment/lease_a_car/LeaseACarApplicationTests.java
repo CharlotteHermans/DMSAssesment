23d868a78c7653b9032525b4ce793490e13cb29a
Charlotte <chermans@svb.nl>
2025-04-30 08:41:12 +0200

    project toevoegen (zit nog verder niks in, test of connectie naar github werkt)

---
+++ lease-a-car/src/test/java/nl/svb/dms/assessment/lease_a_car/LeaseACarApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.svb.dms.assessment.lease_a_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaseACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
